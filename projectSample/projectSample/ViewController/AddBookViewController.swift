99390bd04cde2dc8d004298cf9b893577d3e677c
Xcode User <xcode@Fasts-MacBook-Pro-2.local>
2020-04-13 17:31:56 -0400

    Add a book code added

--- projectSample/projectSample/ViewController/AddBookViewController.swift
+++ projectSample/projectSample/ViewController/AddBookViewController.swift
@@ -8,17 +8,73 @@
 
 import UIKit
 
-class AddBookViewController: UIViewController {
+class AddBookViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet var tfTitle: UITextField!
+    @IBOutlet var tfAuthor: UITextField!
+    @IBOutlet var tfPublisher: UITextField!
+    @IBOutlet var tfGenre: UITextField!
+    
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func chooseImage(_sender: Any)
+    {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        let actionSheet = UIAlertController(title: "Photo source", message: "Choose a source", preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(action:UIAlertAction)in
+            if UIImagePickerController.isSourceTypeAvailable(.camera){
+            imagePickerController.sourceType = .camera
+            self.present(imagePickerController, animated: true, completion: nil)
+            }
+            else
+            {
+                print("Camera not available")
+            }
+        } ))
+      actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: {(action:UIAlertAction)in
+        imagePickerController.sourceType = .photoLibrary
+        self.present(imagePickerController, animated: true, completion: nil)
+      }))
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        imageView.image = image
+        picker.dismiss(animated: true, completion: nil)
+    }
     
     
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+    picker.dismiss(animated: true, completion: nil)
+    }
 
+    @IBAction func addBook(sender: Any)
+    {
+        let mainDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let book : BookData = BookData.init()
+        let imgData = imageView.debugDescription
+        book.initWithValues(id: 0, title: tfTitle.text!, author: tfAuthor.text!, publisher: tfPublisher.text!, genre: tfGenre.text!, image: imgData)
+        
+        mainDelegate.insertDataToBook(title: tfTitle.text!, author: tfAuthor.text!, publisher: tfPublisher.text!, genre: tfGenre.text!, imga: imgData)
+        
+      //  var returnMSG : String = "Book Added"
+    
+        
+    }
     /*
     // MARK: - Navigation
 
